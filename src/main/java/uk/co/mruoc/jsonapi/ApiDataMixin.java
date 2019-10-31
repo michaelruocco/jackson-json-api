161839e3a6619a7f315eb47db7dfc084ecef78d1
Michael Ruocco <michael.ruocco@hotmail.com>
2019-10-31 00:49:43 +0000

    corrects mixins, they were acidentally added with attributes being ignored

--- src/main/java/uk/co/mruoc/jsonapi/ApiDataMixin.java
+++ src/main/java/uk/co/mruoc/jsonapi/ApiDataMixin.java
@@ -1,12 +1,10 @@
 package uk.co.mruoc.jsonapi;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({ "type", "attributes" })
 public interface ApiDataMixin<T> {
 
-    @JsonIgnore
-    T getAttributes();
+    // intentionally blank
 
 }
--- src/main/java/uk/co/mruoc/jsonapi/ApiDataWithIdMixin.java
+++ src/main/java/uk/co/mruoc/jsonapi/ApiDataWithIdMixin.java
@@ -1,12 +1,10 @@
 package uk.co.mruoc.jsonapi;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({ "id", "type", "attributes" })
 public interface ApiDataWithIdMixin<T> {
 
-    @JsonIgnore
-    T getAttributes();
+    // intentionally blank
 
 }
